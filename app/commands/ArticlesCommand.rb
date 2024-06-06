6c414554f1fd79364e3c46cc7561986e9bf22416
kazumasatorii <>
2024-06-07 08:58:17 +0900

    ArticlesCommand class create

---
+++ app/commands/ArticlesCommand.rb
@@ -0,0 +1,17 @@
+class ArticlesCommand
+  def initialize(article, article_params)
+    @article = article
+    @article_params = article_params
+  end
+  def create
+    if @article.valid?(@article_params)
+      @article.save(@article_params)
+    end
+  end
+
+  def update
+    if @article.valid?(@article_params)
+      @article.save(@article_params)
+    end
+  end
+end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  layout false
   def index
   end
 end
